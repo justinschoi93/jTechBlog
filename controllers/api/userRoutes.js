8fb2b1036b6fc64ebaf47651e47ec86688ebd94a
justinschoi93 <justinschoi93@gmail.com>
2023-08-12 09:49:31 -0700

    new-comment form created

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,12 +4,12 @@ const withAuth = require('../../utils/auth');
 
 //POST: api/user
 router.post('/', withAuth, async (req, res)=>{
-    console.log('fetch received')
+    console.log(req.body);
     try   {
         const userData = await User.create(req.body);
-        console.log(userData)
+        
         const users = userData.map(user => user.get({ plain: true}))
-        console.log(users)
+        
         req.session.save(()=>{
             req.session.user_id = userData.id;
             req.session.logged_in = true;
@@ -28,7 +28,7 @@ router.post('/login', async (req, res) => {
     console.log(req.body);
     
     const userData = User.findOne( {where: {username: req.body.username}});
-    console.log(userData);
+    
 
         const passIsValid = await userData.checkPassword(req.body.password);
         console.log(passIsValid);
--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,13 @@
 async function signUpFormHandler (e) {
     e.preventDefault();
-
+    console.log('click')
     const username = document.querySelector('.sign-up-name').value.trim();
     const email = document.querySelector('.sign-up-email').value.trim();
     const password = document.querySelector('.sign-up-password').value.trim();
 
     if (username && email && password) {
+
+        console.log('fetch made');
 
         const response = fetch('/api/user', {
             method: 'POST',
@@ -13,15 +15,10 @@ async function signUpFormHandler (e) {
             headers: {'Content-Type': 'application/json'} 
         })
 
-        console.log('fetch made')
+        console.log('fetch completed')
         console.log(response.ok)
-        // if (response.ok){
-        //     console.log('Sign up successful!');
-        //     document.location.replace('/homepage')
-        // } else {
-        //     document.location.replace('/login')
-        // }
-    }
+        
+    };
 }
 
 async function logInFormHandler (event) {
@@ -31,8 +28,8 @@ async function logInFormHandler (event) {
     const password = await document.querySelector(".log-in-password").value.trim();
     
     if (username && password) {
-        console.log(username, password);
-        // const bodyObject = {username: username, password: password}
+    
+        
         const response = await fetch('/api/user/login', {
             method: 'POST',
             body: JSON.stringify({username: username, password: password}),
@@ -40,17 +37,16 @@ async function logInFormHandler (event) {
         });
 
         console.log('fetch request made')
-        // console.log(response.ok);
+       
         if (response.ok){
             console.log('Log in successful!');
-            // document.location.replace('/homepage')
+      
         } else {
-            // document.location.replace('/login')
+          
         }
 
     }
 }
 
 document.querySelector(".log-in-form").addEventListener("submit", logInFormHandler);
-
 document.querySelector(".sign-up-form").addEventListener("submit", signUpFormHandler)
